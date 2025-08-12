2b5dd8f566abae1fd9a4f9df7974d667c7d89b02
Uyen-nt <147871443+Uyen-nt@users.noreply.github.com>
2025-08-12 23:29:21 +0700

    update settings

--- Careerpath/settings.py
+++ Careerpath/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['careerpath.io.vn', 'www.careerpath.io.vn']
+ALLOWED_HOSTS = ['careerpath.io.vn', 'www.careerpath.io.vn', '103.173.66.148']
 
 
 # Application definition
